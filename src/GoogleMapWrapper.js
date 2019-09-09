9a88dc75cea135b6fb62304d971955e7680610f9
Danny Hyeong Yu <hyeong.yu@mail.utoronto.ca>
2019-09-09 17:05:32 -0400

    Update GoogleMapWrapper.js

--- src/GoogleMapWrapper.js
+++ src/GoogleMapWrapper.js
@@ -1,6 +1,12 @@
 import {GoogleApiWrapper} from 'google-maps-react';
 import MapContainer from './MapContainer.js'
+/**
+ * React Component wrapper around MapContainer
+ * 
+ * @file Using GoogleAPI Wrapper pass APIKey and map object to the MapContainer
+ * @module GoogleApiWrapper
+ */
 
 export default GoogleApiWrapper({
-    apiKey: ("AIzaSyBY1dvS9jsf5VBlZJhkYCNxEj3o0DhI4zM")
+    apiKey: (process.env.REACT_APP_GOOGLE_API_KEY)
 })(MapContainer)
