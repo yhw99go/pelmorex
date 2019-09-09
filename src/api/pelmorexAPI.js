d8f7ef4a516d5ea49daf8f0edf297a2ddf5427f6
Danny Hyeong Yu <hyeong.yu@mail.utoronto.ca>
2019-09-09 14:02:25 -0400

    Create pelmorexAPI.js

---
+++ src/api/pelmorexAPI.js
@@ -0,0 +1,30 @@
+function paramsToQueryString(params) {
+    return Object.keys(params).map((key) => { return key + "=" + params[key] }).join("&");
+}
+
+export function getBoundsWithDetail(ne, sw) {
+    var url = 'process.env.REACT_APP_NOT_SECRET_CODE';
+    var params = {
+            "client_key": "51e05a51-5caf-42db-aedf-d658eb88f2af",
+            "poi_fields" : "id,name,label,center"
+    }
+    var data = {
+        "filters": {
+            "bounding_box": {
+                "top_left_lat": ne.lat(),
+                "top_left_lon": sw.lng(),
+                "bottom_right_lat": sw.lat(),
+                "bottom_right_lon": ne.lng()
+            }
+        }
+    }
+
+    return fetch(url + "?" + paramsToQueryString(params), {
+        method: 'POST',
+        body: JSON.stringify(data), 
+        headers:{
+        'Content-Type': 'application/json'
+        }
+    })
+
+}
