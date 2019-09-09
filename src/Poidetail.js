395d4f5fd2d672a5d266f6d676775363a407a59d
Danny Hyeong Yu <hyeong.yu@mail.utoronto.ca>
2019-09-09 17:05:47 -0400

    Update Poidetail.js

--- src/Poidetail.js
+++ src/Poidetail.js
@@ -1,13 +1,25 @@
 import React from 'react';
+/**
+ * 
+ * @file React Component that displaying poi detail
+ * @module Poidetail
+ * @extends React.Component
+ */
 
 export default class Poidetail extends React.Component {
     constructor(props){ 
         super(props);
     }
+
+/**
+  * render a component
+  * @method
+  * @summary render poi detail data that selected from map
+  * @return {object} 
+  */
     render(){
         return (
             <p>{this.props.location.state.label}</p>
         );
     }
 }
-
