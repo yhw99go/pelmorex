2f24e690455aa0e9ea2a1d009372d3c07326c8a3
Danny Hyeong Yu <hyeong.yu@mail.utoronto.ca>
2019-09-09 17:05:05 -0400

    Update pelmorexAPI.js

--- src/api/pelmorexAPI.js
+++ src/api/pelmorexAPI.js
@@ -1,11 +1,28 @@
+/**
+ * @file Contains API handler
+ */
+
+/**
+  * @method
+  * @summary method to join url and params and return
+  * @param {object} params - parameter for API
+  * @return {String} - return string that include URL and params
+  */
 function paramsToQueryString(params) {
     return Object.keys(params).map((key) => { return key + "=" + params[key] }).join("&");
 }
 
+/**
+  * @method
+  * @summary method to make POST call for permorexAPI
+  * @param {object} ne - location of north east of the map
+  * @param {object} sw - location of sotuh west of the map
+  * @return {object} - return api response
+  */
 export function getBoundsWithDetail(ne, sw) {
-    var url = process.env.REACT_APP_PERMOREX_API;
+    var url = process.env.REACT_APP_PELMOREX_API;
     var params = {
-            "client_key": process.env.REACT_APP_PERMOREX_API_CLIENT_ID,
+            "client_key": process.env.REACT_APP_PELMOREX_API_CLIENT_ID,
             "poi_fields" : "id,name,label,center"
     }
     var data = {
